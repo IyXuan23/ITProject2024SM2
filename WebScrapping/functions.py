1d74a45fa60d909e2cf34ec870585267ab01a831
IyXuan23 <127928542+IyXuan23@users.noreply.github.com>
2024-08-09 20:03:07 +1000

    added code to parse pre-requisites
    Added function parsePreReq to scrape the code for the pre-requisite subjects needed to take a relevant subject, and then add it to the JSON file. Further testing will be needed to check that this scrape will handle all the edge cases for pre-requisite subjects (especially the part where the subjects allows the user to choose from one of several possible prequisites)

--- WebScrapping/functions.py
+++ WebScrapping/functions.py
@@ -28,22 +28,40 @@ def scrapeLinks(numOfPages, targetArray, url):
         print("scrapped page" + str(i))
         i += 1    
 
-def writeJSONFile(filePath, name, code, aims, indicativeContent):
+def writeJSONFile(filePath, name, code, aims, indicativeContent, necessaryPreReq, oneOfPreReq):
     """writes information to JSON file for the corresponding subbject. Naming convention is [subjectCode]_info.json"""
+
+    necessaryStr = ''
+    oneOfStr = ''
+
+    for req in necessaryPreReq:
+        if necessaryStr != '':
+            necessaryStr = necessaryStr + ', '
+        necessaryStr = necessaryStr + req
+
+    for req in oneOfPreReq:
+        if oneOfStr != '':
+            oneOfStr = oneOfStr + ', '
+        oneOfStr = oneOfStr + req    
 
     data = {
         "subject_name": name,
         "subject_code": code,
         "aims": aims, 
-        "indicative_content": indicativeContent
+        "indicative_content": indicativeContent,
+        "necessary pre_requisite": necessaryStr,
+        "one of pre_requisite": oneOfStr
     }
 
     with open(filePath, 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
+def scrapeOverview(url):
+    """function will scrape the subject code and name, overview content and indicative content, and return it as an array
+    note: there may be changes to this function down the line to add in ILOs"""
 
-def scrapSubject(url):
-    """function for scraping a singular subject information, and will return the data in the form of a json file"""
+    #note: url is in the format:
+    #https://handbook.unimelb.edu.au/2024/subjects/comp30022
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -70,11 +88,79 @@ def scrapSubject(url):
     except AttributeError:
         description = None
 
+    return subjectName, subjectCode, aims, indicativeContent
+
+def parseTable(table):
+    """helper function: parse table from html, will return the courses as an array"""
+
+    courses = []
+    rows = table.find_all('tr')
+
+    for row in rows:
+        tds = row.find_all('td')
+        if tds:
+            courseCode = tds[0].text.strip()
+            courses.append(courseCode)
+
+    return courses        
+
+def parsePreReq(courseCode):
+    """function will look through the required pre-requisites of a subject, and return the necessary data accordingly.
+    2 arrays will be returned, one with the subjects that are necessary [singlePreReq], and one where the student as to do (one of)
+    the listed subjects to qualify [oneOfPreReq]"""
+
+    #note: url is in form:
+    #https://handbook.unimelb.edu.au/subjects/comp30022/eligibility-and-requirements
+
+    courseCode = courseCode.lower()
+    preReqURL = 'https://handbook.unimelb.edu.au/subjects/' + courseCode + '/eligibility-and-requirements'
+
+    response = requests.get(preReqURL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    try: 
+        preReqContainer = soup.find('div', id='prerequisites')
+
+        singlePreReq = []
+        oneOfPreReq = []
+
+        paragraphs = preReqContainer.find_all('p')
+
+        for para in paragraphs:
+
+            text = para.get_text(strip=True).lower()
+
+            if "one of" in text:
+                table = para.find('table')
+                if table:
+                    oneOfPreReq.extend(parseTable(table))
+
+            else:
+                table = para.find('table')
+                if table:
+                    singlePreReq.extend(parseTable(table))       
+
+    except AttributeError:
+        print("Not Found")
+
+    return singlePreReq, oneOfPreReq
+
+def scrapSubject(url):
+
+    """function for scraping a singular subject information, and will return the data in the form of a json file"""
+
+    subjectName, subjectCode, aims, indicativeContent = scrapeOverview(url)
+
+    necessaryPreReq, oneOfPreReq = parsePreReq(subjectCode)
+
     #writing to JSON file
     fileName = subjectCode + '_info.json'
     filePath = os.path.join('subjectInfo', fileName)
 
-    writeJSONFile(filePath, subjectName, subjectCode, aims, indicativeContent)
-    
+    writeJSONFile(filePath, subjectName, subjectCode, aims, indicativeContent, necessaryPreReq, oneOfPreReq)
+
+    #https://handbook.unimelb.edu.au/subjects/comp30022/assessment
+    #https://handbook.unimelb.edu.au/2024/subjects/comp30022/dates-times
+    #https://handbook.unimelb.edu.au/subjects/comp30022/further-information
 
 
