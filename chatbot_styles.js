99675243e605d2ee5234adcf4becde7831b74065
Journey Lu-Spencer <jilk456@gmail.com>
2024-08-22 17:06:11 +1000

    Folder can now be loaded into Google Chrome as an extension

---
+++ chatbot_styles.js
@@ -0,0 +1,20 @@
+const elementRef = document.getElementById("chat-element");
+    elementRef.htmlClassUtilities = {
+      ['custom-button']: {
+        events: {
+          click: (event) => {
+            const text = event.target.children[0].innerText;
+            elementRef.submitUserMessage(text.substring(1, text.length - 1));
+          },
+        },
+        styles: {
+          default: {backgroundColor: '#ffffff', borderRadius: '10px', padding: '10px', cursor: 'pointer', textAlign: 'center'},
+          hover: {backgroundColor: '#ebebeb'},
+          click: {backgroundColor: '#e4e4e4'},
+        },
+      },
+      ['custom-button-text']: {styles: {default: {pointerEvents: 'none'}}},
+    };
+
+    elementRef.demo = true;
+    // Setting value via a property (easiest way)
