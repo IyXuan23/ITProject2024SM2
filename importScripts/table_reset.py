0625a3d2ec409251d52b5c5679b02406e5d7457d
Eric <ericnguyen1211@outlook.com>
2024-09-05 16:04:47 +1000

    Improve reset functionality for ease of use

--- importScripts/table_reset.py
+++ importScripts/table_reset.py
@@ -1,24 +1,28 @@
 """
-This file contains script to erase data from a selected table.
+This file contains a script to erase data from a selected table.
 """
-import glob
-import json
-import os
+import argparse
 import psycopg2
 
+# Parse the table name from command-line arguments
+parser = argparse.ArgumentParser(description="Truncate a specific table in the database.")
+parser.add_argument('table_name', type=str, help='The name of the table to truncate.')
+args = parser.parse_args()
 
 # Connect to the database
 conn = psycopg2.connect(database="postgres", user="postgres.seqkcnapvgkwqbqipqqs",
                         password="IT Web Server12", host="aws-0-ap-southeast-2.pooler.supabase.com", port=6543)
 cur = conn.cursor()
 
-#TRUNCATE TABLE [name of the table] CASCADE;
-cur.execute("""TRUNCATE TABLE subjects CASCADE;""")
+# Build and execute the SQL query with the specified table name
+truncate_query = f"TRUNCATE TABLE {args.table_name} CASCADE;"
+cur.execute(truncate_query)
 
-#Commit the transaction    
+# Commit the transaction
 conn.commit()
 
-#Close the cursor
+# Close the cursor and the connection
 cur.close()
 conn.close()
-    
+
+print(f"Table '{args.table_name}' has been truncated.")
