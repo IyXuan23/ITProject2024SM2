d8a525257eacbc73f397771d3972de1972791f20
Eric <ericnguyen1211@outlook.com>
2024-09-07 01:17:56 +1000

    Implement function to convert a list of dinctionaries into a list

--- importScripts/utils.py
+++ importScripts/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 def convert_to_daterange(period_str):
+    #print("Perod_str is" + period_str)
     """
     Converts a period string (e.g., "26 February 2024 to 26 May 2024")
     into a PostgreSQL daterange string (e.g., "[2024-02-26,2024-05-26]").
@@ -23,8 +24,10 @@ def convert_to_daterange(period_str):
 
         # Return the daterange string
         return f'[{start_date},{end_date}]'
+    
+    # Occur if no date range provided.
     except ValueError as e:
-        print(f"Error parsing date range '{period_str}': {e}, using default dates.")
+        print(f"Error parsing date range '{period_str}': {e}, return None. Don't worry, this means a date range isn't provided")
         return None
 
 def convert_to_date(date_str):
@@ -47,5 +50,31 @@ def convert_to_date(date_str):
         return datetime.strptime(date_str.strip(), "%d %B %Y").date()
     except ValueError as e:
         # Handle any ValueError due to improper date formatting
-        print(f"Error parsing date '{date_str}': {e}")
+        print(f"Error parsing date '{date_str}': {e}. Don't worry, this means a date isn't provided")
         return None
+    
+def convert_listdict_to_list(input_list: list) -> None | list:
+    """
+    Converts a list of dictionaries 
+    into a single of list of values from all dictionaries
+    
+    Args:
+    input_list (list): A list of dictionaries.
+    
+    Returns:
+    None if list is empty.
+    """
+    return_list = []
+    for info in input_list:
+        for value in info.values():
+            if value != None:
+                for sub in value:
+                    if isinstance(sub,list):
+                        for elt in sub:
+                            elt = elt +  '; '
+                            return_list.append(elt)
+                    else:
+                        return_list.append(sub)
+                    continue
+    return None if len(return_list) == 0 else return_list
+            
