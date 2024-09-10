d7b4534a3977899cf336bf730b780ec98f9405b9
Eric <ericnguyen1211@outlook.com>
2024-09-10 20:51:35 +1000

    Added functionality to import overview. 'aims' and 'overview' are now imported as text

--- importScripts/subjects_Import.py
+++ importScripts/subjects_Import.py
@@ -5,6 +5,7 @@ import glob
 import json
 import os
 import psycopg2
+from utils import convert_list_into_text
 
 
 # Connect to the database
@@ -32,21 +33,36 @@ for file_path in glob.glob(os.path.join(folder_path, '*.json')):
     content = data.get('indicative content', [])
     if not isinstance(content, list):
         content = []
-
+    else: 
+        content_import = convert_list_into_text(content)
+        
     aims = data.get('aims', [])
     if not isinstance(aims, list):
         aims = []
+    else:
+        aims_import = convert_list_into_text(aims)
 
     learning_outcomes = data.get('intended learning outcomes', [])
     if not isinstance(learning_outcomes, list):
         learning_outcomes = []
+    else:
+        learning_outcomes_import = convert_list_into_text(learning_outcomes)
+
+    overview = data.get('overview',[])
+    if not isinstance(overview,list):
+        overview = []
+    else:
+        overview_import = convert_list_into_text(overview)
+
+    print(overview_import)
+
             
     # Insert information from file to table for each semester
     cur.execute("""
-                INSERT INTO subjects(subject_code, subject_name, aims, indicative_content,subject_level,learning_outcomes)
-                VALUES (%s,%s,%s,%s,%s,%s)
+                INSERT INTO subjects(subject_code, subject_name, aims, indicative_content,subject_level,learning_outcomes,overview)
+                VALUES (%s,%s,%s,%s,%s,%s,%s)
                 ON CONFLICT (subject_code) DO NOTHING """
-                ,(subject_code, subject_name, aims, content, subject_level, learning_outcomes))
+                ,(subject_code, subject_name, aims_import, content_import, subject_level, learning_outcomes_import,overview_import))
     
 #Commit the transaction    
 conn.commit()
