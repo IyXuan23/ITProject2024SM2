6db1760992334c03f3f3ddf51ae58d3461a75ba0
Eric <ericnguyen1211@outlook.com>
2024-09-29 17:19:04 +1000

    Implemented functionality to deal with follow - up questions from the user

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from functools import wraps
-from flask import Flask, jsonify, Response, request
+from flask import Flask, jsonify, Response, request, session
 from flask_cors import CORS
 import flask
 import psycopg2
@@ -9,23 +9,64 @@ from psycopg2 import sql
 from openai import OpenAI
 from cache import MemoryCache
 from dependencies.vanna import VannaDefault
+from followup import *
 
+# TODO: Implement a functionality to manage cookie (session) size
+
+"""
+File:           app.py
+Description:    This file contains all necessary functions for VANNA to generate a SQL query from the user's input
+"""
+
+                            ####################### SET UP #######################
 cache = MemoryCache()
 load_dotenv()
 app = Flask(__name__, static_url_path='')
 
 openai_api_key = os.environ.get('OPENAI_API_KEY')
 vanna_api_key = os.environ.get('VANNA_API_KEY')
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+# For testing, set the secret_key to any number (e.g: '1')
+app.secret_key = '9999'
 
-# SETUP
-vanna_model_name = 	"unimelb-handbook-chatbot"
+
+#vanna_model_name = "unimelbhtesting"
+vanna_model_name = "unimelb-handbook-chatbot"
+
 
 vn = VannaDefault(model=vanna_model_name, api_key=vanna_api_key)
-# vn.connect_to_postgres(dbname="postgres",user="postgres.seqkcnapvgkwqbqipqqs",password="IT Web Server12",host="aws-0-ap-southeast-2.pooler.supabase.com",port=6543)
-# NO NEED TO CHANGE ANYTHING BELOW THIS LINE
 
-def is_sql_valid(sql_query):
+                            ####################### CACHES & COOKIES #######################
+@app.before_request
+def make_session_non_permanent():
+    # Ensure that the session is cleared when the browser is closed
+    session.permanent = False 
+
+def requires_cache(fields):
+    def decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            id = request.args.get('id')
+
+            if id is None:
+                return jsonify({"type": "error", "error": "No id provided"})
+            
+            for field in fields:
+                if cache.get(id=id, field=field) is None:
+                    return jsonify({"type": "error", "error": f"No {field} found"})
+            
+            field_values = {field: cache.get(id=id, field=field) for field in fields}
+            
+            # Add the id to the field_values
+            field_values['id'] = id
+
+            return f(*args, **field_values, **kwargs)
+        return decorated
+    return decorator
+
+
+                            ####################### MAIN FUNCTIONS #######################
+
+def is_sql_valid(sql_query:str) -> bool:
     try:
         # 使用您的数据库连接参数
         conn = psycopg2.connect(
@@ -52,76 +93,76 @@ def is_sql_valid(sql_query):
         if conn:
             conn.close()
 
-def requires_cache(fields):
-    def decorator(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            id = request.args.get('id')
-
-            if id is None:
-                return jsonify({"type": "error", "error": "No id provided"})
-            
-            for field in fields:
-                if cache.get(id=id, field=field) is None:
-                    return jsonify({"type": "error", "error": f"No {field} found"})
-            
-            field_values = {field: cache.get(id=id, field=field) for field in fields}
-            
-            # Add the id to the field_values
-            field_values['id'] = id
-
-            return f(*args, **field_values, **kwargs)
-        return decorated
-    return decorator
-
-@app.route('/api/v1/generate_questions', methods=['GET'])
-def generate_questions():
-    generate_questions = vn.generate_questions()
-    return jsonify({
-        "type": "question_list",
-        "questions": generate_questions,
-        "header": "Here are some questions you can ask:"
-        })
-
-@app.route('/api/v1/generate_sql', methods=['GET'])
+# TODO: Implement a functionality to manage cookie (session) size
+@app.route('/api/v0/generate_sql', methods=['GET'])
 def generate_sql():
     user_question = flask.request.args.get('question')
-    # Step 1: Generate the SQL query from the user question
-    sql = vn.generate_sql(user_question)
-
+    previous_convo = get_conversation()
+    print(previous_convo)
+    
+    if previous_convo:
+        # Determine whether the question is a follow - up question
+        if is_followup_question(previous_convo, user_question):
+            messages = construct_rephrase(previous_convo, user_question)
+            rephrased_question = rephrase_question(messages)
+        
+        # If the user change topic, the convo history will be reset
+        else:
+            print("RESET")
+            previous_convo = [{"role": "system",
+                "content": "You are a university handbook assistant chatbot, you help student find subject information, you are providing formal and precise response corresponding to the user input, you are now asked to convert the following text into a more colloquial response and do not include 'sure', response in markdown format."},]
+            save_conversation(previous_convo)
+            rephrased_question = user_question
+    else:
+        messages=[
+                {"role": "system",
+                "content": "You are a university handbook assistant chatbot, you help student find subject information, you are providing formal and precise response corresponding to the user input, you are now asked to convert the following text into a more colloquial response and do not include 'sure', response in markdown format."},
+                ]
+        previous_convo.extend(messages)
+        save_conversation(previous_convo)
+        rephrased_question = user_question
+    
+    # Generate the SQL query from the user REPHRASED question
+    sql = vn.generate_sql(rephrased_question)
+    print(rephrased_question)
+    print(sql)
     valid = is_sql_valid(sql)
 
     if valid:
+
+        # Fetch info from database
         conn = psycopg2.connect(database="postgres",user="postgres.seqkcnapvgkwqbqipqqs",password="IT Web Server12",host="aws-0-ap-southeast-2.pooler.supabase.com",port=6543)
         cur = conn.cursor()
         cur.execute(sql)
         result = cur.fetchall()
-
         cur.close()
         conn.close()
-
+        
         client = OpenAI(
             api_key=openai_api_key)
-
+        
+        # Pass the user's query and fetched data to LLM
+        previous_convo.append({"role": "user", "content": f"User input: {rephrased_question}"})
+        previous_convo.append({"role": "assistant", "content": f"System output: {result}"})
         response = client.chat.completions.create(
             model="gpt-4o-mini",
-            messages=[
-                {"role": "system",
-                "content": "You are a university handbook assistant chatbot, you help student find subject information, you are providing formal and precise response corresponding to the user input, you are now asked to convert the following text into a more colloquial response and do not include 'sure', response in markdown format."},
-                {"role": "user", "content": f"User input: {user_question}"},
-                {"role": "assistant", "content": f"System output: {result}"}
-            ]
+            messages=previous_convo
         ).choices[0].message.content
-        return jsonify({
-            "response": response
-        })
+        previous_convo.append({"role": "assistant", "content": response})
 
-    # Step 5: Return the OpenAI response as a JSON response
+        # Save conversation
+        save_conversation(previous_convo)
+        
+        # Return the OpenAI response as a JSON response
+        return jsonify({
+            "response": response,
+        })
     return jsonify({
         "response": sql,
     })
 
-@app.route('/api/v1/run_sql', methods=['GET'])
+
+@app.route('/api/v0/run_sql', methods=['GET'])
 @requires_cache(['sql'])
 def run_sql(id: str, sql: str):
     try:
@@ -138,8 +179,95 @@ def run_sql(id: str, sql: str):
 
     except Exception as e:
         return jsonify({"type": "error", "error": str(e)})
+                        
+                        ####################### FIXED FOLLOW - UP QUESTIONS #######################
 
-@app.route('/api/v1/download_csv', methods=['GET'])
+@app.route('/api/v0/generate_questions', methods=['GET'])
+def generate_questions():
+    generate_questions = vn.generate_questions()
+    return jsonify({
+        "type": "question_list",
+        "questions": generate_questions,
+        "header": "Here are some questions you can ask:"
+        })
+
+@app.route('/api/v0/generate_followup_questions', methods=['GET'])
+@requires_cache(['df', 'question', 'sql'])
+def generate_followup_questions(id: str, df, question, sql):
+    followup_questions = vn.generate_followup_questions(question=question, sql=sql, df=df)
+
+    cache.set(id=id, field='followup_questions', value=followup_questions)
+
+    return jsonify(
+        {
+            "type": "question_list", 
+            "id": id,
+            "questions": followup_questions,
+            "header": "Here are some followup questions you can ask:"
+        })
+
+@app.route('/api/v0/load_question', methods=['GET'])
+@requires_cache(['question', 'sql', 'df', 'fig_json', 'followup_questions'])
+def load_question(id: str, question, sql, df, fig_json, followup_questions):
+    try:
+        return jsonify(
+            {
+                "type": "question_cache", 
+                "id": id,
+                "question": question,
+                "sql": sql,
+                "df": df.head(10).to_json(orient='records'),
+                "fig": fig_json,
+                "followup_questions": followup_questions,
+            })
+
+    except Exception as e:
+        return jsonify({"type": "error", "error": str(e)})
+
+                        ####################### TRAINING #######################
+
+@app.route('/api/v0/get_training_data', methods=['GET'])
+def get_training_data():
+    df = vn.get_training_data()
+
+    return jsonify(
+    {
+        "type": "df", 
+        "id": "training_data",
+        "df": df.head(25).to_json(orient='records'),
+    })
+
+@app.route('/api/v0/remove_training_data', methods=['POST'])
+def remove_training_data():
+    # Get id from the JSON body
+    id = flask.request.json.get('id')
+
+    if id is None:
+        return jsonify({"type": "error", "error": "No id provided"})
+
+    if vn.remove_training_data(id=id):
+        return jsonify({"success": True})
+    else:
+        return jsonify({"type": "error", "error": "Couldn't remove training data"})
+
+@app.route('/api/v0/train', methods=['POST'])
+def add_training_data():
+    question = flask.request.json.get('question')
+    sql = flask.request.json.get('sql')
+    ddl = flask.request.json.get('ddl')
+    documentation = flask.request.json.get('documentation')
+
+    try:
+        id = vn.train(question=question, sql=sql, ddl=ddl, documentation=documentation)
+
+        return jsonify({"id": id})
+    except Exception as e:
+        print("TRAINING ERROR", e)
+        return jsonify({"type": "error", "error": str(e)})
+
+                        ####################### UTILS #######################
+
+@app.route('/api/v0/download_csv', methods=['GET'])
 @requires_cache(['df'])
 def download_csv(id: str, df):
     csv = df.to_csv()
@@ -150,7 +278,7 @@ def download_csv(id: str, df):
         headers={"Content-disposition":
                  f"attachment; filename={id}.csv"})
 
-@app.route('/api/v1/generate_plotly_figure', methods=['GET'])
+@app.route('/api/v0/generate_plotly_figure', methods=['GET'])
 @requires_cache(['df', 'question', 'sql'])
 def generate_plotly_figure(id: str, df, question, sql):
     try:
@@ -173,85 +301,22 @@ def generate_plotly_figure(id: str, df, question, sql):
 
         return jsonify({"type": "error", "error": str(e)})
 
-@app.route('/api/v1/get_training_data', methods=['GET'])
-def get_training_data():
-    df = vn.get_training_data()
-
-    return jsonify(
-    {
-        "type": "df", 
-        "id": "training_data",
-        "df": df.head(25).to_json(orient='records'),
-    })
-
-@app.route('/api/v1/remove_training_data', methods=['POST'])
-def remove_training_data():
-    # Get id from the JSON body
-    id = flask.request.json.get('id')
-
-    if id is None:
-        return jsonify({"type": "error", "error": "No id provided"})
-
-    if vn.remove_training_data(id=id):
-        return jsonify({"success": True})
-    else:
-        return jsonify({"type": "error", "error": "Couldn't remove training data"})
-
-@app.route('/api/v1/train', methods=['POST'])
-def add_training_data():
-    question = flask.request.json.get('question')
-    sql = flask.request.json.get('sql')
-    ddl = flask.request.json.get('ddl')
-    documentation = flask.request.json.get('documentation')
-
-    try:
-        id = vn.train(question=question, sql=sql, ddl=ddl, documentation=documentation)
-
-        return jsonify({"id": id})
-    except Exception as e:
-        print("TRAINING ERROR", e)
-        return jsonify({"type": "error", "error": str(e)})
-
-@app.route('/api/v1/generate_followup_questions', methods=['GET'])
-@requires_cache(['df', 'question', 'sql'])
-def generate_followup_questions(id: str, df, question, sql):
-    followup_questions = vn.generate_followup_questions(question=question, sql=sql, df=df)
-
-    cache.set(id=id, field='followup_questions', value=followup_questions)
-
-    return jsonify(
-        {
-            "type": "question_list", 
-            "id": id,
-            "questions": followup_questions,
-            "header": "Here are some followup questions you can ask:"
-        })
-
-@app.route('/api/v1/load_question', methods=['GET'])
-@requires_cache(['question', 'sql', 'df', 'fig_json', 'followup_questions'])
-def load_question(id: str, question, sql, df, fig_json, followup_questions):
-    try:
-        return jsonify(
-            {
-                "type": "question_cache", 
-                "id": id,
-                "question": question,
-                "sql": sql,
-                "df": df.head(10).to_json(orient='records'),
-                "fig": fig_json,
-                "followup_questions": followup_questions,
-            })
-
-    except Exception as e:
-        return jsonify({"type": "error", "error": str(e)})
-
-@app.route('/api/v1/get_question_history', methods=['GET'])
-def get_question_history():
-    return jsonify({"type": "question_history", "questions": cache.get_all(field_list=['question']) })
-
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
+@app.route('/api/v0/get_question_history', methods=['GET'])
+def get_question_history():
+    return jsonify({"type": "question_history", "questions": cache.get_all(field_list=['question']) })
+
+                        ####################### CONTROL #######################
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
---
+++ api/followup.py
@@ -0,0 +1,58 @@
+import os
+from openai import OpenAI
+from flask import session
+
+
+"""
+File:           followup.py
+Description:    This file contains all necessary functions to detect follow - up questions and rephrase them
+"""
+
+openai_api_key = os.environ.get('OPENAI_API_KEY')
+
+def get_conversation ():
+    return session.get('conversation_history',[])
+
+def save_conversation(conversation_history : dict):
+    session['conversation_history'] = conversation_history
+
+def rephrase_question(convo : dict) -> str:
+    response = OpenAI(api_key=openai_api_key).chat.completions.create(
+        model="gpt-4o-mini",
+        messages=convo
+    ).choices[0].message.content
+    return response
+
+def is_followup_question(conversation_history : dict, new_question : str) -> bool:
+    messages = [
+        {
+            "role": "system",
+            "content": "You are an AI assistant that determines if a user's new question is related to the previous conversation."
+        }
+    ]
+    messages.extend(conversation_history)
+    messages.append({
+        "role": "user",
+        "content": (
+            f"Is the following question a follow-up to our previous conversation or a new topic?\n"
+            f"Question: \"{new_question}\"\n"
+            "Two questions are unrelated if they are about different subjects (different subject name or code) or courses\n"
+            "Answer with 'follow-up' or 'new topic'."
+        )
+    })
+    response = OpenAI(api_key=openai_api_key).chat.completions.create(
+        model="gpt-4o",
+        messages=messages
+    ).choices[0].message.content.strip().lower()
+
+    if 'follow-up' in response:
+        return True
+    else:
+        return False
+
+def construct_rephrase(conversation_history : dict, followup_question : str) -> dict:
+    messages = [{"role": "system", "content": "You are an assistant that rephrases questions to include necessary context based on the conversation."}]
+    messages.extend(conversation_history)
+    messages.append({"role": "user", "content": f"Based on the above conversation, rephrase my last question\n\nMy last question: \"{followup_question}\"\nRephrased question:"})
+    return messages
+
