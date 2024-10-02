526d9c6c168083f2f0ad5da0aef461edf1f5cde9
Jason <junquany@student.unimelb.edu.au>
2024-10-02 19:47:27 +1000

    Implement followup-question suggestion

--- chatbot_connect.js
+++ chatbot_connect.js
@@ -12,14 +12,49 @@ document.addEventListener('DOMContentLoaded', async(event) => {
 
     chatElement.responseInterceptor = (response) => {
         result = response.response;
-        response = {text: result};
+        response = { text: result};
         return response;
+    };
+
+    chatElement.onMessage = (message) => {
+        if (message.message.role === 'ai' && !message.isHistory) {
+            chatElement.addMessage({
+                html: `
+                <div class="deep-chat-temporary-message">
+                    <button class="deep-chat-button deep-chat-suggestion-button" id = "suggestion1" style="margin-top: 5px"></button>
+                    <button class="deep-chat-button deep-chat-suggestion-button" id = "suggestion2" style="margin-top: 6px"></button>
+                    <button class="deep-chat-button deep-chat-suggestion-button" id = "suggestion3" style="margin-top: 6px"></button>
+                </div>`, 
+                role: "user"
+            });
+            const suggestion1 = chatElementRef.shadowRoot.querySelector('#suggestion1')
+            const suggestion2 = chatElementRef.shadowRoot.querySelector('#suggestion2')
+            const suggestion3 = chatElementRef.shadowRoot.querySelector('#suggestion3')
+            const suggestion = getRndSuggestion();
+            suggestion1.textContent = suggestion[0]
+            suggestion2.textContent = suggestion[1]
+            suggestion3.textContent = suggestion[2]
+            console.log(message.message.html);
+        }
     };
 });
 
-chatElement.validateInput = (text) => {
-    return text.length > 0;
-};
+
+function getRndSuggestion() {
+    const suggestions = [
+        "Tell me what's the prerequisite for this subject?",
+        "What are the learning outcomes for this subject?",
+        "How many assessments does this subject have?",
+        "Who is the coordinator for this subject?",
+        "What are the teaching times for this subject?",
+        "When is the census date for this subject?",
+    ]
+    while (suggestions.length < 6) {
+        var r = Math.floor(Math.random() * suggestions.length) + 1;
+        if (arr.indexOf(r) === -1) arr.push(r);
+    }
+    return suggestions;
+}
 
 chatElementRef.htmlClassUtilities = {
     'custom-button': {
@@ -39,7 +74,8 @@ chatElementRef.htmlClassUtilities = {
                 cursor: 'pointer',
                 textAlign: 'center',
                 marginTop: '10px',
-                boxshadow: '0 0 0 5px rgba(0, 0, 0, 0.5)'
+                boxshadow: '0 0 0 5px rgba(0, 0, 0, 0.5)',
+                border: '1px solid #969696'
             },
             hover: { backgroundColor: '#ebebeb', transform: 'scale(1.03)', transition: 'transform 0.3s ease' },
             click: { backgroundColor: '#e4e4e4', transform: 'scale(1.03)', transition: 'transform 0.3s ease' },
