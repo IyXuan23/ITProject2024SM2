da07ac735256fa03312b8269604afbc48e7befda
Jason <junquany@student.unimelb.edu.au>
2024-10-03 12:00:58 +1000

    Fixed minor bugs

--- api/app.py
+++ api/app.py
@@ -179,11 +179,11 @@ def run_sql(id: str, sql: str):
 
 @app.route('/api/v0/generate_questions', methods=['GET'])
 def generate_questions():
-    question = flask.request.args.get('question')
-    generate_questions = vn.get_similar_question_sql(question=question)
+    generate_questions = vn.generate_questions()
     return jsonify({
         "type": "question_list",
         "questions": generate_questions,
+        "header": "Here are some questions you can ask:"
         })
 
 @app.route('/api/v0/generate_followup_questions', methods=['GET'])
