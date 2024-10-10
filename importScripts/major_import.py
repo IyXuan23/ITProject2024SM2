1e915564c1ede97d062ba22c958b1c03a51b6a18
Eric <ericnguyen1211@outlook.com>
2024-10-10 15:50:01 +1100

    Implemented script to import majors

---
+++ importScripts/major_import.py
@@ -0,0 +1,53 @@
+"""
+This file contains script to import scraped data to "majors" table.
+"""
+import glob
+import json
+import os
+import psycopg2
+from utils import *
+
+
+
+# Connect to the database
+conn = psycopg2.connect(database="postgres", user="postgres.seqkcnapvgkwqbqipqqs",
+                        password="IT Web Server12", host="aws-0-ap-southeast-2.pooler.supabase.com", port=6543)
+cur = conn.cursor()
+cur.execute("""TRUNCATE TABLE majors CASCADE;""")
+
+folder_path = 'majorInfo'
+
+
+# Open file and load data from file
+for file_path in glob.glob(os.path.join(folder_path, '*.json')):
+    with open(file_path) as file:
+        data = json.load(file)
+    
+    
+    # Get subject code from file
+    major_name = data['major name']
+
+    course_code = None
+
+    # For debugging
+    print("Importing data for " + major_name)
+
+    overview = convert_list_into_text(data['overview'])
+    ILOs = data['ILOs']
+    structure = json.dumps(data['structure'])
+
+    # Insert information from file to table for each prerequisite option
+    cur.execute("""
+        INSERT INTO majors(overview,learning_outcomes,course_code,major_name,major_structure)
+            VALUES (%s, %s, %s, %s, %s)""", 
+            (overview,ILOs,course_code,major_name,structure))
+            
+   
+#Commit the transaction    
+conn.commit()
+
+#Close the cursor
+cur.close()
+conn.close()
+
+print("--------SUCCEED IN IMPORTING DATA!-----------\n")    
