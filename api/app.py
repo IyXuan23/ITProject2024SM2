5eccd2800723d2e3611533c71db373be26ee8264
Eric Nguyen <Ericnguyen1211@outlook.com>
2024-10-28 21:00:35 +1100

    Improved LLM prompts and removed redundant functions.

--- api/app.py
+++ api/app.py
@@ -15,7 +15,7 @@ from dependencies.correct import correct_subject
 
 """
 File:           app.py
-Description:    This file contains all necessary functions for VANNA to generate a SQL query from the user's input
+Description:    This file contains all necessary functions for Flask to execute when the user send "GET" request and VANNA training functions.
 """
 
                             ####################### SET UP #######################
@@ -98,66 +98,48 @@ def is_sql_valid(sql_query:str) -> bool:
 def generate_sql():
     user_question = flask.request.args.get('question')
     previous_convo = get_conversation()
-    print(previous_convo)
-    
-    keywords = correct_subject(user_question)
 
-    if ("again" not in keywords) and ('course' not in keywords) and ('major' not in keywords):
-        keywords = keywords + '(subject)'
-    
-    print(keywords)
+    # Determine if this is a new conversation
     if previous_convo:
         # Determine whether the question is a follow - up question
-        if is_followup_question(previous_convo, user_question):
-            messages = construct_rephrase(previous_convo, user_question)
-            rephrased_question = rephrase_question(messages)
-        
-        # If the user change topic, the convo history will be reset
-        else:
+        if not is_followup_question(previous_convo, user_question):
+            # If not, reset the conversation history then rephrase user's question 
             print("RESET")
             previous_convo = []
-            if ("again" not in keywords):
-                messages = construct_rephrasev2(previous_convo, user_question,keywords)
-                rephrased_question = rephrase_question(messages)
-            else:
-                messages = construct_rephrase(previous_convo, user_question)
-                rephrased_question = rephrase_question(messages)
+            messages = construct_rephrase(previous_convo, user_question)
+            rephrased_question = rephrase_question(messages)
             previous_convo = [{"role": "system",
-                "content": "You are a university handbook assistant chatbot, you help student find subject information, " 
+                "content": "You are a university handbook assistant chatbot, you help student find information, " 
                 "you are providing formal and precise response corresponding to the user input, " 
                 "you are now asked to convert the following text into a more colloquial response and do not include 'sure', response in markdown format. " 
                 "Please use the context provided in the system output to answer " 
                 "(If system output is empty or none, inform user that there is no available information)."}]
             save_conversation(previous_convo)
-    else:
-        if ("again" not in keywords):
-                messages = construct_rephrasev2(previous_convo, user_question,keywords)
-                rephrased_question = rephrase_question(messages)
         else:
-                messages = construct_rephrase(previous_convo, user_question)
-                rephrased_question = rephrase_question(messages)
+            messages = construct_rephrase(previous_convo, user_question)
+            rephrased_question = rephrase_question(messages)
+    else:
+        messages = construct_rephrase(previous_convo, user_question)
+        rephrased_question = rephrase_question(messages)
         messages=[
                 {"role": "system",
-                "content": "You are a university handbook assistant chatbot, you help student find subject information, " 
+                "content": "You are a university handbook assistant chatbot, you help student find information, " 
                 "you are providing formal and precise response corresponding to the user input, " 
                 "you are now asked to convert the following text into a more colloquial response and do not include 'sure', response in markdown format. " 
                 "Please use the context provided in the system output to answer "
                 "(If system output is empty or none, inform user that there is no available information)."}]
         previous_convo.extend(messages)
         save_conversation(previous_convo)
-        
-
+    
     # Generate the SQL query from the user REPHRASED question
     sql = vn.generate_sql(rephrased_question)
+
+    # Debug 
+    print(previous_convo)  
+    print(rephrased_question)
+    print(sql)
+  
     valid = is_sql_valid(sql)
-
-    # Debugging    
-    print("The rephrased question is: " + rephrased_question)
-    print("The validity of sql is: " + str(valid))
-    print("The sql query is: ")
-    print(str(sql))
-
-    #generate_popup_query(rephrased_question)
     if valid:
         
         # Establish the database connection
@@ -188,130 +170,38 @@ def generate_sql():
         cur.close()
         conn.close()
 
-        #subject_name = correct_subject(rephrased_question)
-        #print(subject_name)
         # Remove columns that have no data (all values are None or empty)
         filtered_result_dict = {}
         for col_name, data_list in result_dict.items():
             if any(value not in (None, '', []) for value in data_list):
                 filtered_result_dict[col_name] = data_list
 
-        # print the processed dict
+        # Debug
         print(filtered_result_dict)
 
         client = OpenAI(
             api_key=openai_api_key)
+      
         # Pass the user's query and fetched data to LLM
         previous_convo.append({"role": "user", "content": f"User input: {rephrased_question}"})
         previous_convo.append({"role": "assistant", "content": f"System output: {filtered_result_dict}"})
-
-
         response = client.chat.completions.create(
             model="gpt-4o",
-            messages=previous_convo,
+            messages=previous_convo
         ).choices[0].message.content
         previous_convo.append({"role": "assistant", "content": response})
-            
-        previous_convo = [
-            msg for msg in previous_convo 
-            if not (msg["role"] == "assistant" and msg["content"] == f"System output: {filtered_result_dict}")]
-        
-    
-    if (valid == False) or len(filtered_result_dict) == 0:
-        print("No SQL query generated. Activated back-up protocol")
-        client = OpenAI(
-            api_key=openai_api_key)
-        # Pass the user's query and fetched data to LLM
-        previous_convo.append({"role": "assistant", "content": f"System output: Use the informations above (ONLY INFORMATIONS FROM ABOVE) to answer. Inform user that there is no available information if no information is found"})
 
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            messages=previous_convo,
-        ).choices[0].message.content
-        previous_convo.append({"role": "assistant", "content": response})
-            
-        previous_convo = [
-            msg for msg in previous_convo 
-            if not (msg["role"] == "assistant" and msg["content"] == f"System output: Use the informations above (AND ONLY INFORMATIONS FROM ABOVE) to answer. Inform user that there is no available information if no information is found")
-        ]
-        
-    # Save conversation
-    save_conversation(previous_convo)
+        # Save conversation
+        save_conversation(previous_convo)
+
+        # Return the OpenAI response as a JSON response
+        return jsonify({
+            "response": response,
+        })
     return jsonify({
-        "response": response,
+        "response": "Sorry, I can't provide any information. Please try again.",
     })
-
-
-@app.route('/api/v0/run_sql', methods=['GET'])
-@requires_cache(['sql'])
-def run_sql(id: str, sql: str):
-    try:
-        df = vn.run_sql(sql=sql)
-
-        cache.set(id=id, field='df', value=df)
-
-        return jsonify(
-            {
-                "type": "df", 
-                "id": id,
-                "df": df.head(10).to_json(orient='records'),
-            })
-
-    except Exception as e:
-        return jsonify({"type": "error", "error": str(e)})
                         
-                        ####################### FIXED FOLLOW - UP QUESTIONS #######################
-
-@app.route('/api/v0/generate_questions', methods=['GET'])
-def generate_questions():
-    generate_questions = vn.generate_questions()
-    return jsonify({
-        "type": "question_list",
-        "questions": generate_questions,
-        "header": "Here are some questions you can ask:"
-        })
-
-@app.route('/api/v0/generate_popup_questions', methods=['GET'])
-def generate_popup_questions():
-    user_question = flask.request.args.get('question')
-    popup_questions = generate_popup_query(user_question)
-    return jsonify({
-        "questions": popup_questions,
-    })
-
-
-@app.route('/api/v0/generate_followup_questions', methods=['GET'])
-@requires_cache(['df', 'question', 'sql'])
-def generate_followup_questions(id: str, df, question, sql):
-    followup_questions = vn.generate_followup_questions(question=question, sql=sql, df=df)
-
-    cache.set(id=id, field='followup_questions', value=followup_questions)
-
-    return jsonify(
-        {
-            "type": "question_list", 
-            "id": id,
-            "questions": followup_questions,
-            "header": "Here are some followup questions you can ask:"
-        })
-
-@app.route('/api/v0/load_question', methods=['GET'])
-@requires_cache(['question', 'sql', 'df', 'fig_json', 'followup_questions'])
-def load_question(id: str, question, sql, df, fig_json, followup_questions):
-    try:
-        return jsonify(
-            {
-                "type": "question_cache", 
-                "id": id,
-                "question": question,
-                "sql": sql,
-                "df": df.head(10).to_json(orient='records'),
-                "fig": fig_json,
-                "followup_questions": followup_questions,
-            })
-
-    except Exception as e:
-        return jsonify({"type": "error", "error": str(e)})
 
                         ####################### TRAINING #######################
 
@@ -398,30 +288,7 @@ def root():
 def get_question_history():
     return jsonify({"type": "question_history", "questions": cache.get_all(field_list=['question']) })
 
-@app.route('/api/v0/retrieve_keywords', methods=['GET'])
-def retrieve_keywords():
-    user_input = flask.request.args.get('query')
-    keywords = correct_subject(user_input)
-    if '(' in keywords and ')' in keywords:
-        try:
-            type = keywords.split('(')[1].split(')')[0].strip()
-            keywords = keywords.split('(')[0].strip()
-        except IndexError:
-            return jsonify({"error": "Invalid format in query"})
-    else:
-        return jsonify({"error": "Expected format missing parentheses"})
-    
-    return jsonify({"type": str(type), "keywords": str(keywords)})
-
-
                         ####################### CONTROL #######################
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
